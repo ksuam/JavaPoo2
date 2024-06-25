901cd470b0395b70c20f49751a46704c0aff8c54
ksuam <ksuamartinez@gmail.com>
2024-06-24 19:53:12 -0500

    clases

--- src/palabrathis/PalabraThis.java
+++ src/palabrathis/PalabraThis.java
@@ -5,6 +5,8 @@ public class PalabraThis {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Juan", "Perez");
         System.out.println("persona = " + persona1);
+        System.out.println("persona nombre = " + persona1.nombre);
+
     }
 }
 
@@ -13,8 +15,20 @@ class Persona{ //Solo se puede acceder dentro del paquete
     String nombre;
     String apellido;
     
-    Persona (String nombre, String apellido){
+    Persona (String nombre, String apellido){ //constructor del objeto
         this.nombre = nombre;
         this.apellido = apellido;
+        System.out.println("OBjeto perosna usando this"+this); // Imprimitr el mismo objeto desde si mismo
+        new Imprimir().imprimir(this); // Otra sitaxis para crear un objeto
     }
+}
+
+class Imprimir{
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde imprimir"+ persona);
+        System.out.println("Persona desde imprimir"+ this);
+
+    }
+     
+           
 }
