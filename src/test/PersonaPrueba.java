33e86ec724d19c1adcc8775781a6ff1ead88e162
ksuam <ksuamartinez@gmail.com>
2024-07-04 11:04:19 -0500

    n

--- src/test/PersonaPrueba.java
+++ src/test/PersonaPrueba.java
@@ -5,12 +5,12 @@ import dominio.Persona;
 public class PersonaPrueba {
 
     public static void main(String[] args) {
-        Persona persona1 = new Persona("Juan", 5000, false);
+        Persona persona1 = new Persona("Juan");
 
         persona1.setNombre("Juan carlos");
         System.out.println("persona1 = " + persona1.getNombre());
         System.out.println("persona1 eliminado = " + persona1.isEliminado());
 
-        System.out.println("persona1 objeto= " + persona1.toString());
+        System.out.println("persona1 objeto= " + persona1.toStrin());
     }
 }
